cd49bae6777655449c6c0bd886ec6c431c68429d
ClaySamuelC <claysamuelc@gmail.com>
2022-06-04 11:16:23 -0700

    Add babel loading to build out of es6

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   mode: 'development'
 }
