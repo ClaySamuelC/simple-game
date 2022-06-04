5ba46b3cee2ca12d6bb1510963d64843c6d13264
ClaySamuelC <claysamuelc@gmail.com>
2022-06-04 14:12:58 -0700

    Update character view to be more friendly to styling

--- src/views/character.jsx
+++ src/views/character.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const Character = (props) => {
   return (
-    <div>
-      <h1>Name: {props.name}</h1>
-      <h1>Health: {props.health}</h1>
+    <div className="character">
+      <div>Name: {props.name}</div>
+      <div>Health: {props.health}</div>
     </div>
   );
 };
